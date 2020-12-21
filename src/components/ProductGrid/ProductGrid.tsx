7538159f69a7faa132247c1666610ce8261365ae
rubenk <ruben.kuiper@justbrands.nl>
2020-12-21 09:09:31 +0100

    final commit

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,28 +1,26 @@
-import * as React from 'react'
+import * as React from "react";
 
-import products from '../Api/Products'
-import ProductCard from '../ProductCard'
-import styles from './ProductGrid.module.scss'
-
+import products from "../Api/Products";
+import ProductCard from "../ProductCard";
+import styles from "./ProductGrid.module.scss";
 
 const ProductGrid = () => {
-    
-    return (
-        <div className={styles.productGridWrapper}>
-            <div className={styles.productGrid}>
-                {/* <div className={styles.productCardFillerStart}></div> */}
-                {products.map((product, index) => {
-                    return <ProductCard 
-                        productId={product.productId} 
-                        productName={product.name} 
-                        position={(index % 2 === 0) ? 'right' : 'left'}
-                        key={index}
-                        />
-                })}
-                {/* <div className={styles.productCardFillerEnd}></div> */}
-            </div>
-        </div>    
-    )
-}
+  return (
+    <div className={styles.productGridWrapper}>
+      <div className={styles.productGrid}>
+        {products.map((product, index) => {
+          return (
+            <ProductCard
+              productId={product.productId}
+              productName={product.name}
+              position={index % 2 === 0 ? "right" : "left"}
+              key={index}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ProductGrid
+export default ProductGrid;
